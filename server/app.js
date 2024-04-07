dd108e1d64f9f0a8727348cd82da51247aeefaa7
ysayonnar <ysayonnar@gmail.com>
2024-04-07 21:12:37 +0300

    setted connection to database

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path');
 const fileUpload = require('express-fileupload')
+const sequelize = require('./db')
 require('dotenv').config()
 
 const app = express()
@@ -12,6 +13,16 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-})
+const start = async() => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start()
