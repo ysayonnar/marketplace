62771d0bff3285b072c883b490b68c6fc494777c
ysayonnar <ysayonnar@gmail.com>
2024-04-23 00:18:27 +0300

    сделал карточки товаров

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,9 +1,58 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import ui from '../static/glass.png'
+
 function ProductCard({product}){
-    return(
-        <div className="product_card">
-            <h1>{product.id}</h1>
-        </div>
-    )
+    const [img, setImg] = useState(null )
+    const [path, setPath] = useState(`http://localhost:3000/${product['image_url']}`)
+
+    return (
+			<div className='product_card'>
+				<img className='product_image' src={path} alt='' />
+				<h4
+					style={{
+						fontSize: '24px',
+						margin: '5px 10px',
+					}}
+				>
+					{product.title}
+				</h4>
+				<p
+					style={{
+						width: '220px',
+						margin: '0 10px',
+						fontSize: '14px',
+						color: 'gray',
+					}}
+				>
+					{product.description.slice(0, 25) + '...'}
+				</p>
+				<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+					<h2
+						style={{
+							margin: '5px 10px',
+							color: '#24c211',
+							opacity: '0.7',
+							fontSize: '22px',
+						}}
+					>
+						{product.price} BYN
+					</h2>
+					<h2
+						style={{
+							margin: '6px 10px',
+							color: 'gray',
+							opacity: '0.7',
+							fontSize: '20px',
+                            textDecoration: 'line-through',
+                            textDecorationThickness: '2px'
+						}}
+					>
+						{Math.floor(product.price * 0.75)} BYN
+					</h2>
+				</div>
+			</div>
+		)
 }
 
 export default ProductCard
